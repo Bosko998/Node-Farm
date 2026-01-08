e1ad40cbcc548f04f678e40103a402641e5ab4fe
Bosko998 <boskonikolic1998@gmail.com>
2026-01-08 18:00:18 +0100

    new file:   .prettierrc 	new file:   index.js 	new file:   modules/replaceTemplate.js 	new file:   package.json 	new file:   templates/template-card.html 	new file:   templates/template-overview.html 	new file:   templates/template-product.html 	new file:   txt/append.txt 	new file:   txt/final.txt 	new file:   txt/input.txt 	new file:   txt/output.txt 	new file:   txt/read-this.txt 	new file:   txt/start.txt

---
+++ index.js
@@ -0,0 +1,79 @@
+const fs = require('fs'); //file system module
+const http = require('http');
+const replaceTemplate = require('./modules/replaceTemplate.js');
+const slugify = require('slugify');
+///////////////////////////////////////
+//FILES
+
+//Blocking synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt','utf-8');
+// console.log(textIn);
+// const textOutput = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt',textOutput);
+// console.log('File written!');
+
+//Non-blocking
+// fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
+//     if(err) return console.log('ERROR!');
+
+// fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2)=>{
+//     console.log(data2);
+// fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data3)=>{
+// console.log(data3);
+
+// fs.writeFile('./txt/final.txt', `${data2}\n${data3}` ,'utf-8',err=>{
+// console.log('Your file has been written');
+// })
+// })
+// })
+// })
+// console.log('hello');
+
+///////////////////////////////////////
+//SERVER
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+const server = http.createServer((req, res) => {
+  const myURL = new URL(req.url, `http://${req.headers.host}`);
+  const pathName = myURL.pathname;
+  if (pathName === '/overview' || pathName === '/') {
+    res.writeHead(202, { 'Content-type': 'text/html' });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end('output');
+  } else if (pathName === '/product') {
+    const id = myURL.searchParams.get('id'); // retreive product number
+    const product = dataObj[id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  } else if (pathName === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
+});
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to request on port 8000');
+});
